1a6993bcefe70ed6cd29ff4585ed3e1e35963630
thanos vassilakis <thanos.vassilakis@rbccm.com>
2014-12-19 17:36:38 -0500

    added some comments

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// import and create the server app
+// setting the requests handler to handler.
+
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 var request = require('request');
  
+// now listen for any requests. 
 app.listen(3000);
 
 var spin =0;
@@ -18,15 +22,23 @@ function handler (req, res) {
 	});
 }
  
+
+// now set up the websocket connection handler.
+// 
 io.on('connection', function(socket){
 	console.log('a user connected');
-	collect(socket);
-	var timer = setInterval(function() {
+	collect(socket); // we are connected go and get some data and send it.
+
+	// set up some timer threads.
+	var timer = setInterval(function() { 
 			collect(socket);
-		}, 35000);
+		}, 35000); // do this every 35 seconds.
 	var ping = setInterval(function() {
+		// broadcast ping
 			socket.emit('ping', { spin: spin++});
-		}, 3000);
+		}, 3000); // send  a ping every 3 seconds
+
+	// if we disconnect kill the timer threads.	 
 	socket.on('disconnect', function () {
 		clearInterval(timer);
 		clearInterval(ping);
@@ -38,13 +50,16 @@ io.on('connection', function(socket){
 function collect(socket) {
 	console.log('in collect');
 	var url = 'http://www.google.com/finance/info?client=ig&q=AAPL,N,GOOG,MSFT'
+	// requst for google quotes.
 	request({
 		url: url,
 		proxy: 'http://10.38.89.25:8080'}, function (error, response, body) {
+			// if no error and all is well  process the data
 		if (!error && response.statusCode == 200) {
-			// skipp leading // then parse
+			// skip leading // then parse
 			var quotes = JSON.parse(body.substring(3));
 			console.log(quotes);
+			// broadcast  the data to all the clients
 			socket.emit('quotes', { quotes: quotes, timestamp: new Date().getTime()});
 		} else {
 		console.log(error);
